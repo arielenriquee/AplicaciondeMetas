becc39282995cc65ee1e4c63aa347b57021bb07c
Ariel Oleas Hernandez <arieloleas@hotmail.com>
2026-01-04 13:26:11 +0100

    cambios al estado del formulario

--- src/Componentes/nueva/detalles.jsx
+++ src/Componentes/nueva/detalles.jsx
@@ -1,39 +1,64 @@
+import { useEffect, useState } from "react";
 import estilos from "./detalles.module.css"
 function Detalles () {
+
+    const [form,setForm] = useState({
+        detalles : '',
+        eventos : 1 ,
+        periodo : 'dia',
+        icono : '🏃',
+        meta : 52 ,
+        plazo : '2030-01-01',
+        completado : 0
+    });
+    const {detalles,eventos,periodo,icono,meta,plazo,completado} = form;
+
+    const onChange = (event , prop) => {
+        setForm(estado => ({...estado,[prop]:event.target.value}));
+        
+    }
+    useEffect(()=>{
+        console.log(form);
+    },[form])
+    
+    const crear = async () => {
+        console.log(form);
+    }
+
     const opcionesDeFrecuencia = [ "día" , "semana" , "mes" , "año"];
     const iconos = ["💻","🏃","📚","✈️","💵"];
     return ( 
         <div className="tarjeta">
         <form className="p-4">
             <label className="label">Describe tu meta
-                <input placeholder="ej. 52 caminatas" className="input"/>
+                <input placeholder="ej. 52 caminatas" className="input" value={detalles} onChange={e => onChange(e , 'detalles')}/>
             </label>
             <label className="label" >¿Con que Frecuencia deseas Cumplir tu meta? <span>(ej.1 vez a la semana)</span>
                 <div className="flex mb-6">
-                    <input type="number" className="input mr-6" />
-                    <select className="input">
+                    <input type="number" className="input mr-6" value={eventos} onChange={e => onChange(e , 'eventos')} />
+                    <select className="input" value={periodo} onChange={e => onChange(e , 'periodo')}>
                         {opcionesDeFrecuencia.map(opcion => <option value={opcion}>{opcion}</option>)}
                     </select>
                 </div>
             </label>
             <label className="label" >¿Cuantas veces deseas completar esta meta?
-            <input type="number" className="input" />
+            <input type="number" className="input" value={meta} onChange={e => onChange(e , 'meta')}/>
             </label>
             <label className="label">¿tienes una fecha límite?
-            <input type="date" className="input"/>
+            <input type="date" className="input" value={plazo} onChange={e => onChange(e , 'plazo')}/>
             </label>
-            <label className="label">¿Cunatas veces haz Completado ya esta Meta?
-            <input type="number" className="input"/>
+            <label className="label">¿Cuantas veces haz Completado ya esta Meta?
+            <input type="number" className="input" value={completado} onChange={ e=> onChange(e,'completado')}/>
             </label>
             <label className="label"> Escoge el icono para la meta
-                <select className="input">
+                <select className="input" value={icono} onChange={e => onChange(e , 'icono')}>
                     {iconos.map(opcion => <option value={opcion}>{opcion}</option> )}
                 </select>
                     
             </label>
         </form>
         <div className={estilos.botones}>
-            <button className="boton boton--negro">Cear</button>
+            <button className="boton boton--negro" onClick={crear}>Cear</button>
             <button className="boton boton--gris">Cancelar</button>
         </div>
         
